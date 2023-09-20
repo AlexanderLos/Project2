747c30407c66ad6f4e9aacc59239ad867a6a49db
Illowah <illowahplease@gmail.com>
2023-09-20 18:51:26 -0400

    Setting up project

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 // IMPORTS
 const express = require('express')
 const app = express()
-
 require('dotenv').config()
-
 const PORT = process.env.PORT || 3000
+
+// // MIDDLEWARE
+// app.use(express.json())
+// app.use(express.urlencoded({extended: true}))
+// app.use((req, res, next) => {
+//   console.log(`Request made to url: ${req.url}`)
+//   next()
+// })
 
 // setup database 
 const mongoose = require('mongoose')
@@ -20,9 +26,15 @@ db.on('connected', () => { console.log('mongo connected')})
 db.on('disconnected', () => { console.log('mongo disconnected')})
 
 app.get('/', (req, res) => {
-   res.send('Hello world!')
+   res.render(`daywin.ejs`)
 })
 
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT: ${PORT}`)
 })
+
+// Index Route
+app.get('/', (req, res) => {
+    res.send('Welcome To Day Win.')
+})
+
