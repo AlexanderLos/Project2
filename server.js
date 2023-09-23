e2cd7613de5294cdeff7cf58163361f353c060ac
Illowah <illowahplease@gmail.com>
2023-09-23 03:06:32 -0400

    Added finishing touches to project, show route, nav bar added to each, built in background timer

--- server.js
+++ server.js
@@ -1,81 +1,104 @@
 // IMPORTS
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const PORT = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 const { Task, Progress } = require('./models/daywin');
 const methodOverride = require('method-override');
 
 // MIDDLEWARE
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
 
 app.use((req, res, next) => {
-  console.log(`Request made to url: ${req.url}`)
-  next()
-})
+  console.log(`Request made to URL: ${req.url}`);
+  next();
+});
 
-//Setup Database.
-const mongoose = require('mongoose')
-const mongoURI = process.env.MONGO_URI
+// Setup Database
+const mongoose = require('mongoose');
+const mongoURI = process.env.MONGO_URI;
 
-//Connection to mongo.
-mongoose.connect(mongoURI)
+// Connection to MongoDB
+mongoose.connect(mongoURI);
 
-const db = mongoose.connection
-db.on('error', (err) => { console.log('ERROR: ' , err)})
-db.on('connected', () => { console.log('mongo connected')})
-db.on('disconnected', () => { console.log('mongo disconnected')})
+const db = mongoose.connection;
+db.on('error', (err) => { console.log('ERROR:', err); });
+db.on('connected', () => { console.log('MongoDB connected'); });
+db.on('disconnected', () => { console.log('MongoDB disconnected'); });
+
+
+// Function to start a recurring 24-hour timer
+function the24HourTimer() {
+  setInterval(async () => {
+    const allTasks = await Task.find({});
+    if (allTasks.every(task => task.status === 'Completed')) {
+      await Progress.create({ result: 'Green Day' });
+    } else {
+      await Progress.create({ result: 'Red Day' });
+    }
+    await Task.deleteMany({});
+  }, 60 * 1000); // 1 minute for testing! Check README to see how to change it back to a day.
+}
+
+
+the24HourTimer();
+
+// ROUTES
 
 // Normal Route (Homepage)
 app.get('/', (req, res) => {
-    res.render('daywin.ejs');
+  res.render('daywin.ejs');
 });
 
 // Index Route (List all tasks)
 app.get('/tasks', async (req, res) => {
-    const allTasks = await Task.find({});
-    res.render('tasks.ejs', { Task: allTasks });
+  const allTasks = await Task.find({});
+  res.render('tasks.ejs', { Task: allTasks });
 });
 
 // Create Route (Add new task to the database)
 app.post('/tasks', async (req, res) => {
-    const newTask = await Task.create({
-        taskName: req.body.taskName,
-        dueDate: new Date(),
-        status: 'Pending',
-        dayResult: 'Pending'
-    });
-    res.redirect('/tasks');
+  await Task.create({
+    taskName: req.body.taskName,
+    status: 'Pending'
+  });
+  res.redirect('/tasks');
 });
 
-// Show Route 
+// Progress Route
+app.get('/progress', async (req, res) => {
+  const allProgress = await Progress.find({});
+  res.render('progress.ejs', { progressDays: allProgress });
+});
+
+// Show Route
 app.get('/tasks/:id', async (req, res) => {
-    const task = await Task.findById(req.params.id);
-    res.render('show.ejs', { task: task });
+  const task = await Task.findById(req.params.id);
+  res.render('show.ejs', { task });
 });
 
-// Edit Route 
+// Edit Route
 app.get('/tasks/:id/edit', async (req, res) => {
     const task = await Task.findById(req.params.id);
-    res.render('edit.ejs', { task: task });
+    res.render('edit.ejs', { task });
 });
 
 // Update Route 
 app.put('/tasks/:id', async (req, res) => {
     const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.redirect(`/tasks/${updatedTask.id}`);
+    res.redirect('/tasks');  
 });
 
-// Delete Route 
+// Delete Route
 app.delete('/tasks/:id', async (req, res) => {
-    await Task.findByIdAndDelete(req.params.id);
-    res.redirect('/tasks');
+  await Task.findByIdAndDelete(req.params.id);
+  res.redirect('/tasks');
 });
 
 // Server Listening
 app.listen(PORT, () => {
-    console.log(`Server is listening on PORT: ${PORT}`);
+  console.log(`Server is listening on PORT: ${PORT}`);
 });
